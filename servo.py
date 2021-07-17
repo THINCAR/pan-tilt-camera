d161c96a72dc2cfd46b4d6ae0bea000600f04822
Seo <gusaud2199@naver.com>
2021-07-17 17:25:18 +0900

    Update : initialize variables about channel(horizental, vertical)

--- servo.py
+++ servo.py
@@ -14,10 +14,14 @@ class Servo:
     ang_hor_max = 180
     ang_ver = 90
     ang_hor = 90
+    channel_hor = 0
+    channel_ver = 1
 
 
     def __init__(self):
         self.kit = ServoKit(channels=16)
+        self.set_angle(channel_hor,ang_hor)
+        self.set_angle(channel_ver,ang_ver)
 
     # 서보모터 각도 변경 함수 (채널, 각도)
     def set_angle(self,channel,ang):
@@ -28,25 +32,25 @@ class Servo:
         self.ang_hor -= ang
         if self.ang_hor < self.ang_hor_min:
             self.ang_hor = self.ang_hor_min
-        self.set_angle(0,self.ang_hor)
+        self.set_angle(channel_hor,self.ang_hor)
 
     # 서보모터 우로 회전 (각도)
     def rotate_right(self,ang):
         self.ang_hor += ang
         if self.ang_hor > self.ang_hor_max:
             self.ang_hor = self.ang_hor_max
-        self.set_angle(0,self.ang_hor)
+        self.set_angle(channel_hor,self.ang_hor)
 
     # 서보모터 위로 회전 (각도)
     def rotate_up(self,ang):
         self.ang_ver += ang
         if self.ang_ver > self.ang_ver_max:
             self.ang_ver = self.ang_ver_max
-        self.set_angle(1,self.ang_ver)
+        self.set_angle(channel_ver,self.ang_ver)
 
     # 서보모터 아래로 회전 (각도)
     def rotate_down(self,ang):
         self.ang_ver -= ang
         if self.ang_ver < self.ang_ver_min:
             self.ang_ver = self.ang_ver_min
-        self.set_angle(1,self.ang_ver)
+        self.set_angle(channel_ver,self.ang_ver)
